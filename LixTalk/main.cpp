35dab7188af8b6a84b7cfc4a1d75dc6b3deb4734
ZingLix <zinglix@localhost.localdomain>
2018-10-20 16:15:36 +0800

    add cmake

--- LixTalk/main.cpp
+++ LixTalk/main.cpp
@@ -7,18 +7,17 @@
 #include "LogInfo.h"
 
 int main() {
-	std::cout << "pid:"<<getpid()<<", tid:"<<CurThread::tid()<<std::endl;
+    std::cout << "pid:" << getpid() << ", tid:" << CurThread::tid() << std::endl;
 
-	//ChatServer server(9981);
-	//server.start();
+    ChatServer server(9981);
+    server.start();
 
-	int cnt = 5000000;
-	while (cnt--) {
-		LOG_INFO << "asdf" << "??" << std::to_string(cnt) << "ieie";
-	}
+    int cnt = 500;
+    while (cnt--) {
+        LOG_INFO << "asdf" << "??" << std::to_string(cnt) << "ieie";
+    }
 
 
 
-	std::cin.get();
+    std::cin.get();
 }
-
