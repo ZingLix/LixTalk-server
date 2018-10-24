be4c471b36d10a51e1356a2d68b9338502a4f94b
ZingLix <roadmankael@gmail.com>
2018-10-24 11:57:42 +0800

    now server works

--- LixTalk/main.cpp
+++ LixTalk/main.cpp
@@ -10,18 +10,8 @@
 int main() {
     std::cout << "pid:" << getpid() << ", tid:" << CurThread::tid() << std::endl;
 
- //   ChatServer server(9981);
-//    server.start();
-	std::time_t t = std::time(nullptr);
-	std::cout  << std::put_time(std::gmtime(&t), "%c %Z") << '\n';
+    ChatServer server(9981);
+    server.start();
 
-    int cnt = 5000000;
-    while (cnt--) {
-        LOG_INFO << "asdf" << "??" << std::to_string(cnt) << "ieie";
-    }
-
-	std::time_t t2 = std::time(nullptr);
-	std::cout  << std::put_time(std::gmtime(&t2), "%c %Z") << '\n';
-
-    std::cin.get();
+	return 0;
 }
